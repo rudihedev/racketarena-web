bde0150340cd6424b5a1ebe612ebe6d30ec354d8
rudihedev <rudihedev@gmail.com>
2026-03-17 09:37:59 +0700

    feat: remove example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@/components/ui/button"
-import { getProducts } from "./modules/common/api"
 
 export function App() {
-  getProducts()
   return <Button />
 }
 
--- src/modules/common/api.ts
+++ src/modules/common/api.ts
@@ -1,10 +1,6 @@
 import createClient from "openapi-fetch"
 import type { paths } from "@/schema"
 
-const client = createClient<paths>({ baseUrl: import.meta.env.VITE_API_URL })
-
-export async function getProducts() {
-  const { data: products, error } = await client.GET("/products")
-
-  console.log({ products, error })
-}
+export const fetchClient = createClient<paths>({
+  baseUrl: import.meta.env.VITE_API_URL,
+})
