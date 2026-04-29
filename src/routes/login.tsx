b41eb4b2f34c3890aaa183afcacd92a156bc0189
rudihedev <rudihedev@gmail.com>
2026-04-29 10:34:21 +0700

    feat: login submit to api

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,7 +1,32 @@
+import { LoginForm } from "@/components/auth/login-form"
+import { $api } from "@/modules/common/api"
+
 export function LoginRoute() {
+  const { mutate } = $api.useMutation("post", "/auth/login")
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    const email = formData.get("email")?.toString()
+    const password = formData.get("password")?.toString()
+
+    if (!email || !password) {
+      return
+    }
+
+    mutate({
+      body: {
+        email,
+        password,
+      },
+    })
+  }
+
   return (
     <main>
-      <h1>Login</h1>
+      <LoginForm handleSubmit={handleSubmit} />
     </main>
   )
 }
